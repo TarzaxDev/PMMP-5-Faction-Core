212b8ab2771f524c760637a91b65de135aa1c694
TarzaxDev <98848694+TarzaxDev@users.noreply.github.com>
2023-06-06 22:37:48 +0200

    Test command added

---
+++ src/Tarzax/Commands/TForm.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tarzax\Commands;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
+use Vecnavium\FormsUI\SimpleForm;
+
+class TForm extends Command
+{
+
+    public function __construct()
+    {
+        parent::__construct("tform", "custom form", "/tform", []);
+        $this->setPermissions(["TForm.use"]);
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args)
+    {
+        if($sender instanceof Player)
+        {
+            $this->TForm($sender);
+        } else {
+            $sender->sendMessage("you are not player");
+        }
+    }
+
+    public function TForm($sender)
+    {
+        $form = new SimpleForm(function (Player $sender, $data)
+        {
+            if($data = null){
+                return;
+            }
+            switch($data)
+            {
+                case 0:
+                    break;
+            }
+        });
+        $form->setTitle("TForm");
+        $form->setContent("content");
+        $form->addButton("leave");
+        $form->sendToPlayer($sender);
+    }
+}
