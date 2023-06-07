2c86cd1790ff4a87089ef17e7ac863a5e99d7e3b
TarzaxDev <98848694+TarzaxDev@users.noreply.github.com>
2023-06-07 22:11:24 +0200

    modified file(s) & added file(s)

--- src/Tarzax/Utils.php
+++ src/Tarzax/Utils.php
@@ -2,9 +2,22 @@
 
 namespace Tarzax;
 
+use pocketmine\player\Player;
+use pocketmine\utils\Config;
+
 class Utils {
 
     public static function messageError(): string {
         return "§4ERROR";
+    }
+
+    public static function getPlayerName($player): string {
+        if ($player instanceof Player) {
+            return $player->getName();
+        }
+    }
+
+    public static function getConfigYML(string $filename) {
+        return new Config(Core::getInstance()->getDataFolder() . $filename, Config::YAML);
     }
 }
